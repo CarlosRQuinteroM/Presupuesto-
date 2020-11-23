6f092a5e66405d0d6b3d1cfc560e510ed8a3a88e
CarlosRQuinteroM <carlosquintero92.cq@gmail.com>
2020-11-23 16:21:05 +0100

    1.. almecenando el prosupuesto

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Pregunta from './components/Pregunta'
+import Formulario from './components/Formulario'
 
 function App() {
 
@@ -19,9 +20,18 @@ function App() {
           guardarPresupuesto={guardarPresupuesto}
           guardarRestante= {guardarRestante}
         />
-      </div>
 
-    </header>
+        <div className="row">
+        <div className="one-half column">
+           <Formulario />
+          </div>
+
+               <div className="one-half column">
+            2
+               </div>
+            </div>
+          </div>
+      </header>
     </div>
   );
 }
